8fb84cb0f4708108cf5679daea154cfab0ebd40c
Sara Darwish <38770404+SaraDrwish@users.noreply.github.com>
2023-07-11 23:40:01 +0300

    Revert "contact page"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom"
+
 import Mentee from './components/Mentee';
 import Mentor from './components/Mentor';
 import Opportunities from './components/Opportunities';
 import Selectlist from './components/selectList';
+import { Routes, Route } from "react-router-dom"
 import Requests from './components/Requests';
-import ContactUs from './components/ContactUs';
 function App() {
   return (
     <>
@@ -16,7 +16,6 @@ function App() {
           <Route path='/2' element={<Opportunities/>}/>
           <Route path='/3' element={<Requests/>}/>
         </Route>
-        <Route path='/contactus' element={<ContactUs/>}/>
       </Routes>
     </>
   );
